63806dd66e7f81017f7d7f6309245c9a3f63a0e4
Lindsay Ciastko <LindsayCiastko@gmail.com>
2019-04-11 19:34:23 -0700

    Adding shells for all of the components
    Created component files, and added shell react code to establish a quick render.

---
+++ components/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, Stylesheet } from "react-native";
+
+class Button extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Button</Text>
+      </View>
+    );
+  }
+}
+
+export default Button;
---
+++ components/Placeholder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, Stylesheet } from "react-native";
+
+class Placeholder extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Placeholder</Text>
+      </View>
+    );
+  }
+}
+
+export default Placeholder;
